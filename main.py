db92db6fb2fb4ccd90ce551c8c52fdd51012b783
Aidan M <aidanmurphy.social@pm.me>
2025-11-12 17:47:39 -0600

    Update color

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot, colors
 #           `-    \`_`"'- meow
 
 class c_Grid:
-    def __init__(self, grid:numpy.array, colorset:list=['#FF0000','#00FF00','#0000FF']):
+    def __init__(self, grid:numpy.array, colorset:list=['#000000','#FF0000','#0000FF']):
         self._Index = 0
         self._InitialState = grid
         self._CurrentState = self._InitialState
@@ -17,7 +17,7 @@ class c_Grid:
         print(self._CurrentState)
         if show_plot == True:
             fig,ax1 = pyplot.subplots(nrows=1,ncols=1)
-            ax1.pcolor(self._CurrentState, cmap=self._ColorSet, edgecolor='#000000', alpha=0.5)
+            ax1.pcolor(self._CurrentState, cmap=self._ColorSet, edgecolor='#000000', alpha=0.7)
             fig.savefig(f"./out/graph_{self._Index}")
 
     def update(self):
