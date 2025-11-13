70fd6ba80379ff223d7502f5a621b7f2095e08f1
Aidan M <aidanmurphy.social@pm.me>
2025-11-12 18:27:27 -0600

    Find closest light function

--- moth.py
+++ moth.py
@@ -1,1 +1,20 @@
 import numpy
+
+#Returns tuple(x,y) delta to the nearest light 
+def findClosestLight(grid:numpy.ndarray, mothX:int, mothY:int):
+    shape = grid.shape
+    for i in range(-shape[0],shape[0]):
+        if (mothX+i >= shape[0]) or (mothX+i < 0):
+            continue
+
+        for j in range(-shape[1],shape[1]):
+            if (mothY+j >= shape[1]) or (mothY+j < 0):
+                continue
+
+            if (grid[mothX+i][mothY+j] == 1):
+                return (i,j)
+                
+def nextMove(grid:numpy.ndarray, mothX:int, mothY:int):
+    deltaX, deltaY = 0,0
+
+    return (deltaX, deltaY)
